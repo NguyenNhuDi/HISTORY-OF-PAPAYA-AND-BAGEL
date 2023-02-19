398e52cc5e403834cf1848a7a9e8b34a3c0e4e9f
BartuO <bartuokan1@gmail.com>
2023-02-19 01:20:46 -0700

    PlayerAnimation

--- Assets/PLayerMovement.cs
+++ Assets/PLayerMovement.cs
@@ -7,24 +7,32 @@ public class PLayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private BoxCollider2D coll;
+    private Animator anim;
+    private float dirX;
+    private SpriteRenderer sprite;
 
     // field to pick
     [SerializeField] private LayerMask block;
     [SerializeField] private LayerMask jumpableGround;
+    
+    private enum MovState {idle, running, jumping, falling, walljump};
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
 
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
 
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
 
         if(dirX != 0){
             rb.velocity = new Vector2(dirX *6, rb.velocity.y);
@@ -33,6 +41,8 @@ public class PLayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && (IsGrounded() || StickyLeft() || StickyRight())){
             rb.velocity = new Vector2(rb.velocity.x, 16f);
         }
+
+        UpdateAnimationState();
 
 
         // setXQuad(rb.position.x);
@@ -47,13 +57,38 @@ public class PLayerMovement : MonoBehaviour
 
     private bool StickyRight()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.right, .2f, block);
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.right, .2f, block);     
     }
 
     private bool StickyLeft()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.left, .2f, block);
     }
+
+
+    private void UpdateAnimationState(){
+
+        
+
+        MovState state = MovState.idle;
+        if ((dirX > 0f)){
+                state = MovState.running;
+                sprite.flipX = false;
+        }else if ((dirX < 0f)){
+                state = MovState.running;
+                sprite.flipX = true;
+        }else{
+                state = MovState.idle;
+        }
+
+        if(rb.velocity.y > 0.0001f){
+            state = MovState.jumping;
+        }else if(rb.velocity.y < -0.0001f){
+            state = MovState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+        }
 
 
 }
